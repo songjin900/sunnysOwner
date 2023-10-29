0c32e956480e10e3c7177f731ab36f784ba76cfd
“song” <eke5404@gmail.com>
2023-10-29 18:14:37 -0400

    related Product

--- components/relatedProducts.tsx
+++ components/relatedProducts.tsx
@@ -16,18 +16,18 @@ export default function RelatedProducts({
                 <div className="text-2xl font-medium">Similar items</div>
                 : null
             }
-            <div className="grid lg:grid-cols-4 lg:grid-rows-1 grid-cols-2 grid-rows-2 ">
+            <div className="grid lg:grid-cols-4 lg:grid-rows-1 grid-cols-2 grid-rows-2 space-x-4">
                 {relatedProducts.slice(0, 4).map((pro) => (
                     <Link key={pro.id} legacyBehavior href={`/products/${pro.id}`}>
-                        <div className="m-1 p-2 flex flex-col rounded-2xl cursor-pointer">
+                        <div className="m-1 p-2 flex flex-col rounded-2xl cursor-pointer overflow-hidden">
                             <Image
                                 width={200}
                                 height={200}
                                 src={`https://imagedelivery.net/F5uyA07goHgKR71hGfm2Tg/${pro.image}/relatedProducts`}
-                                className="rounded-2xl"
+                                className="pb-4 mb-2 border-b-2"
                                 alt=""
                             />
-                            <div className="mt-2 pl-4 border-t-2 p-1">{pro.name}</div>
+                            <div className="pl-4 p-1">{pro.name}</div>
                             <div className="pl-4">${pro.price}</div>
                         </div>
                     </Link>
