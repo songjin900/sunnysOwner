e707c8a4ae7d8963daff2e356d62e3ca48c0ebef
“song” <eke5404@gmail.com>
2023-11-03 19:56:16 -0400

    item updates

--- components/items.tsx
+++ components/items.tsx
@@ -6,7 +6,6 @@ interface ItemProps {
     id: number;
     price: number;
     image: string;
-    stockQuantity: number
 }
 //shadow-md shadow-gray-400
 
@@ -14,14 +13,13 @@ export default function Item({
     title,
     price,
     id,
-    image,
-    stockQuantity
+    image
 }: ItemProps) {
     return (//bg-[#F4F4F4]
         <Link legacyBehavior href={`/products/${id}`}>
             <a className="bg-white p-1 rounded-xl shadow-gray-300 cursor-pointer">
-                <div className="flex flex-col h-full">
-                    <div className="flex items-end justify-center flex-1 pb-1">
+                <div className="flex flex-col">
+                    <div className="flex justify-center flex-1 items-start pb-2 border-b-2">
                         <Image
                             src={`https://imagedelivery.net/F5uyA07goHgKR71hGfm2Tg/${image}/shopPage`}
                             className="bg-white rounded-xl border-gray-200"
@@ -31,10 +29,10 @@ export default function Item({
 
                         />
                     </div>
-                    <div className={`flex flex-col pl-1 border-t-2 border-gray-200 overflow-hidden ${stockQuantity > 0 ? "": "bg-red-300"}`}>
-                        <span className="text-sm md:text-lg font-md text-black pl-1 mt-2 truncate-text truncate">{title}</span>
+                    <div className="flex flex-col pl-1 border-gray-200 pt-1 overflow-hidden flex-1">
+                        <span className={`font-md text-black pl-1 truncate-text truncate 
+                            ${title.length > 30 ? 'lg:text-base md:text-sm text-xs': 'lg:text-lg md:text-base text-sm'}`}>{title}</span>
                         <span className="text-xs md:text-sm text-black pl-1">${price}</span>
-                        <span className="text-xs md:text-sm text-black pl-1">Stock: {stockQuantity}</span>
                     </div>
                 </div>
             </a>
